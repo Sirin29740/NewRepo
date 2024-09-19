9b0bdcda9d23a77ecb1c087b22446d004ced5ec1
Sirin29740 <109429698+Sirin29740@users.noreply.github.com>
2024-09-19 17:48:45 +0800

    Update and rename 00.cpp to 字符串转换整数(atoi).cpp

--- Project1/字符串转换整数(atoi).cpp
+++ Project1/字符串转换整数(atoi).cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int myAtoi(string s) {
         int len=s.length();
-        int result=0;
+        long long result=0;
         int iszheng=1;
         int i=0;
         while(i<len)
         {
             if(s[i]==' ')i++;
+            else break;
         }
         if(s[i]=='-'){iszheng=-1;i++;}
         else if(s[i]=='+'){i++;}
         while(i<len)
         {
             if(s[i]>'9'||s[i]<'0')break;
-            if(result>INT_MAX/10){break;}
+           
             result=result*10+(s[i]-'0');
+            long long v=result*iszheng;
+           
+            if(v>=INT_MAX){return INT_MAX;}
+            if(v<=INT_MIN){return INT_MIN;}
             i++;
         }
         
