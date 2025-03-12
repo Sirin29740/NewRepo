0e503acb7f2471df18c24858cf70be3aa8901dda
Sirin29740 <109429698+Sirin29740@users.noreply.github.com>
2025-03-12 16:08:56 +0800

    Create 不同路径.cpp

---
+++ 不同路径.cpp
@@ -0,0 +1,45 @@
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。问总共有多少条不同的路径？
+
+ //动态规划
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> f(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+};
+//用滚动数组代替方案一的二维数组
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> f(n,1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                f[j]+=f[j-1];
+            }
+        }
+        return f[n - 1];
+    }
+};
+//数学方法
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return ans;
+    }
+};
