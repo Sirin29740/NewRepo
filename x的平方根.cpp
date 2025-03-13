9194b78b4ef2e8fec36521116f6178b63aeddfc1
Sirin29740 <109429698+Sirin29740@users.noreply.github.com>
2025-03-13 18:56:11 +0800

    Create x的平方根.cpp

---
+++ x的平方根.cpp
@@ -0,0 +1,36 @@
+//二分法
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long long)mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+//牛顿迭代法
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        double C = x, x0 = x;
+        while (true) {
+            double xi = 0.5 * (x0 + C / x0);
+            if (fabs(x0 - xi) < 1e-7) {
+                break;
+            }
+            x0 = xi;
+        }
+        return int(x0);
+    }
+};
