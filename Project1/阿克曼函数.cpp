45f493643fd57ae11c8c6f85aebc69c5d8ab8929
Sirin29740 <kiroto29740@163.com>
2025-03-22 10:42:11 +0800

    change

---
+++ Project1/阿克曼函数.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+int Ackerman(int m ,int n )
+{
+	if (m == 0)return n + 1;
+	else if (n == 0)return Ackerman(m - 1, 1);
+	else return Ackerman(m - 1, Ackerman(m, n - 1));
+}
+
+
+int main()
+{
+	int m, n;
+	cout << "请输入m:";
+	cin >> m;
+	cout << endl;
+	cout << "请输入n:";
+	cin >> n;
+	cout << endl;
+	cout<<"结果为："<<Ackerman(m, n)<<endl;
+	system("pause");
+	return 0;
+}
