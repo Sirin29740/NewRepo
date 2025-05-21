a0be5d10d6dacc9dd749dda235633a4b278db35d
Sirin29740 <109429698+Sirin29740@users.noreply.github.com>
2025-05-21 17:37:11 +0800

    Create 最大数.go

---
+++ Golang/最大数.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func largestNumber(nums []int) string {
+	sort.Slice(nums, func(i, j int) bool {
+		x, y := nums[i], nums[j]
+		sx, sy := 10, 10
+		for sx <= x {
+			sx *= 10
+		}
+		for sy <= y {
+			sy *= 10
+		}
+		return sy*x+y > sx*y+x
+	})
+	if nums[0] == 0 {
+		return "0"
+	}
+	ans := []byte{}
+	for _, x := range nums {
+		ans = append(ans, strconv.Itoa(x)...)
+	}
+	return string(ans)
+}
+
+func main() {
+	nums := []int{10, 20, 50, 30, 5, 2222, 43}
+	fmt.Println(largestNumber(nums))
+}
