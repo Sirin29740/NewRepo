7193ecb55f6ca05cde2ff0b4a9b9a28e29238236
Sirin29740 <109429698+Sirin29740@users.noreply.github.com>
2025-09-03 10:47:37 +0800

    Create 中序遍历.go

---
+++ Golang/中序遍历.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+    var inorder func(node *TreeNode)
+    var res []int
+    inorder = func(node *TreeNode){
+        if node == nil {
+            return
+        }
+        inorder(node.Left)
+        res = append(res,node.Val)
+        inorder(node.Right)
+    }
+    inorder(root)
+    return res
+}
