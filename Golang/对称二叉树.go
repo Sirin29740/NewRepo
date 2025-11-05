b155ef3e7c5c009eb3e97be0723151de94ba25ab
Sirin29740 <109429698+Sirin29740@users.noreply.github.com>
2025-11-05 19:15:43 +0800

    Create 对称二叉树.go

---
+++ Golang/对称二叉树.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+    return check(root.Left,root.Right)
+}
+func check(p,q *TreeNode) bool {
+    if p == nil && q == nil {
+        return true 
+    }
+    if p == nil || q == nil {
+        return false
+    }
+    return p.Val == q.Val && check(p.Left,q.Right) && check(p.Right,q.Left)
+}
